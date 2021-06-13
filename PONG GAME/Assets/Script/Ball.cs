5f284bd6c968a44058cd3559efbb2610d4ff5788
bobettes1 <kenzelkdsouza@gmail.com>
2021-06-13 17:07:38 +0400

    Uploaded Pong Game

---
+++ PONG GAME/Assets/Script/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{   
+    public float speed;
+    public Rigidbody2D rb;
+    public Vector2 startPosition;
+    private float x , y ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        launch(); 
+    }
+
+    private void launch ()
+    {
+        x = Random.Range(0,2) == 0 ? -1 : 1 ;
+        y = Random.Range(0,2) == 0 ? -1 : 1 ;
+        rb.velocity = new Vector2 (speed *x , speed *y);
+
+    }
+    // Update is called once per frame
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+        launch();
+    }
+}
---
+++ PONG GAME/Assets/Script/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool isPlayer1Goal;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Ball"))
+            if (isPlayer1Goal)
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player1Scored();
+            else 
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player2Scored();
+    }
+}
